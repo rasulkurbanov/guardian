6d3fb84153f8bb1f4422519a599fe85504357c5b
rasulkurbanov <rasuljohn139@gmail.com>
2021-10-05 03:06:20 +0500

    Improving static files

--- guardian/urls.py
+++ guardian/urls.py
@@ -19,7 +19,6 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
